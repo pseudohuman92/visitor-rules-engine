c34c57b5151a5fe197e25150ff20329c5fa1d8c4
Atalay Ileri <atalay@mit.com>
2019-02-13 16:46:45 -0500

    update proto compiler script for client

--- webclient/bin/gen-proto-js.sh
+++ webclient/bin/gen-proto-js.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-proto_dir="../src/main/java/com/ccg/ancientaliens/protocol"
+proto_dir="../src/main/proto"
 ./bin/protoc --js_out=import_style=commonjs,binary:src/proto --proto_path=${proto_dir} ${proto_dir}/*.proto
 for f in src/proto/*.js ; do 
   sed -i '1i/* eslint-disable */\' ${f}
