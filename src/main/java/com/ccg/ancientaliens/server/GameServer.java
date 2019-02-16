97071cf165832d72e8e180941500384b8bfb3fce
Atalay Ileri <atalay@mit.edu>
2019-02-15 23:06:27 -0500

    fix null pointer exception

--- src/main/java/com/ccg/ancientaliens/server/GameServer.java
+++ src/main/java/com/ccg/ancientaliens/server/GameServer.java
@@ -35,6 +35,7 @@ public class GameServer {
         tables = new Hashmap<>();
         chatLog = new ArrayList<>();
         games = new Hashmap<>();
+        gameQueue = new ArrayList<>();
     }
 
 
