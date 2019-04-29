fcf20bfd062d1caaa91336f4a4f3f70ad7da1de6
Atalay Ileri <atalay@mit.com>
2019-04-29 17:04:02 -0400

    Add .env to ignore

---
+++ webclient/src/Components/Fonts/Fonts.jsx
@@ -0,0 +1,25 @@
+
+import GoogleFontLoader from 'react-google-font-loader';
+
+import React, { Component } from 'react';
+
+export default class Fonts extends Component {
+    render() {
+        return (
+            <div>
+                <GoogleFontLoader
+      fonts={[
+        {
+          font: 'Open Sans',
+          weights: [400, 700],
+        },
+        {
+          font: 'Quicksand',
+          weights: [400, 700],
+        },
+      ]}
+    />
+            </div>
+        )
+    }
+}
